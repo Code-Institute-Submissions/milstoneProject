89cc2021c8f7f11b0c0b2543a298c3ca6c04d596
Ubuntu <ubuntu@ip-172-31-92-94.ec2.internal>
2019-06-28 15:02:22 +0000

    init reCaptcha

--- sbcontact.php
+++ sbcontact.php
@@ -1,16 +1,32 @@
-<?php $name = $_POST['name'];
+<?php
+if(isset($_POST['g-recaptcha-response'])) {
+   // RECAPTCHA SETTINGS
+   $captcha = $_POST['g-recaptcha-response'];
+   $ip = $_SERVER['REMOTE_ADDR'];
+   $key = '6Ld3IKsUAAAAAIv5Ar5GzE4Wwta_ooLF1rCDAUxZ';
+   $url = 'https://www.google.com/recaptcha/api/siteverify';
+ 
+   // RECAPTCH RESPONSE
+   $recaptcha_response = file_get_contents($url.'?secret='.$key.'&response='.$captcha.'&remoteip='.$ip);
+   $data = json_decode($recaptcha_response);
+ 
+   if(isset($data->success) &&  $data->success === true) {
+       // code goes here
+
+$name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$formcontent="Från: $name \n meddelande: $message";
+$formcontent="From: $name \n meddelande: $message";
 $recipient = "rawa_aref@hotmail.com";
-$subject = "Lead Sälja bil";
+$subject = "Lead Salja Bil";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 header("Location: contactsuccess.html");
+
+
+   }
+   else {
+      header("Location: contactfail.html");
+   }
+}
 ?>
-
-
-
-
-
- 
